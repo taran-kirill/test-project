af4aba62cae1adb4c7d829f9d831ae5fa0c209f2
taran-kirill <taran050911@gmail.com>
2018-02-23 19:49:40 +0200

    Version 0.0.1

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	"use strict";
+	var swiper = new Swiper('.swiper-container-discount', {
+		slidesPerView: 4,
+		spaceBetween: 30,
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev'
+	});
+
+});
+
