2005e8501f7715161dcb5955d015adffba6ab05e
taran-kirill <taran050911@gmail.com>
2018-02-25 14:12:34 +0200

    version 0.0.0.2

--- js/common.js
+++ js/common.js
@@ -4,7 +4,18 @@ $(document).ready(function () {
 		slidesPerView: 4,
 		spaceBetween: 30,
 		nextButton: '.swiper-button-next',
-		prevButton: '.swiper-button-prev'
+		prevButton: '.swiper-button-prev',
+		 breakpoints: {
+            600: {
+                slidesPerView: 1,
+            },
+            950: {
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 3,
+            }
+        }
 	});
 
 });
